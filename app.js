6cb27a47cc00ad17e6940765079b1b4ece7e1880
Brooklyne Cheney <brookecheney@hotmail.com>
2019-01-23 13:48:25 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const server = http.createServer((req, res) => {
     if (url === '/') {
         res.setHeader('Content-type', 'text/html');
         res.write('<html>');
+        res.write('<h1>Hello from my Node.js Server!</h1>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
@@ -22,14 +23,15 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
         const parsedBody = Buffer.concat(body).toString();
         const message = parsedBody.split('=')[1];
-        fs.writeFileSync('message.txt', message);
+        fs.writeFileSync('message.txt', message, (error) => {
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();  
+        });
+      
     });
-   
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+}
 
-    }
     res.setHeader('Content-type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
